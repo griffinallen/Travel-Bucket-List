91254aa9748ee1f1162f0a7f8595c2dcf3bcebf2
gallen1999 <gallen1999@live.com>
2020-10-31 20:02:59 -0400

    First commit

---
+++ js/map.js
@@ -0,0 +1,112 @@
+var mymap = L.map('map').setView([51.505, -0.09], 2);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2FsbGVuMTk5OSIsImEiOiJja2d3cmI5bWkwY3pxMzFwbmgwMmhlajk4In0.ZPpCyzJ_4pGPb2ujRkxqbQ', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    minZoom: 2,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ2FsbGVuMTk5OSIsImEiOiJja2d3cmI5bWkwY3pxMzFwbmgwMmhlajk4In0.ZPpCyzJ_4pGPb2ujRkxqbQ'
+}).addTo(mymap);
+
+var sidebar = L.control.sidebar({
+    autopan: false,
+    closeButton: true,    // whether to add a close button to the panes
+    container: 'sidebar', // the DOM container or #ID of a predefined sidebar container that should be used
+    position: 'left',     // left or right
+}).addTo(mymap);
+
+sidebar.addPanel({
+    id:   'Home',
+    tab:  '<i class="fas fa-bars"></i>',
+    title: 'Home',
+    pane: '<ul id="ulCoordinates"></ol>',
+}).addPanel({
+    id:   'js-api',
+    tab:  '<i class="fas fa-cog"></i>',
+    title: 'JS API',
+    pane: '<p>The Javascript API allows to dynamically create or modify the panel state.<p/><p><button onclick="sidebar.enablePanel(\'mail\')">enable mails panel</button><button onclick="sidebar.disablePanel(\'mail\')">disable mails panel</button></p><p><button onclick="addUser()">add user</button></b>',
+}).addPanel({
+    id:   'mail',
+    tab:  '<i class="fa fa-envelope"></i>',
+    title: 'Messages',
+    button: function() { alert('opened via JS callback') },
+    disabled: true,
+})
+
+
+
+
+
+var markers = L.layerGroup().addTo(mymap);
+var popup = L.popup();
+readCoordinates();
+
+
+
+
+/**
+ * read all the bucket list items from the database
+ */
+async function readCoordinates() {
+    try {
+        
+        const result = await fetch("http://localhost:8080/coordinates", { method: "GET" });
+        const coordinates = await result.json();
+        coordinates.forEach(t => {
+            addCoordinateToList(t);
+        });
+    }
+    catch (e) {
+        console.log("error reading coordinates")
+    }
+
+}
+
+
+async function addCoordinateToList(coordinate){
+    console.log(coordinate);
+    const ulCoordinates = document.getElementById("ulCoordinates")
+    const li = document.createElement("li")
+    li.textContent = coordinate.id + ": " + coordinate.x + ", " + coordinate.y + ", " + coordinate.name + ", " + coordinate.visited;
+    li.id = coordinate.id;
+    var marker = L.marker([coordinate.x, coordinate.y]);
+    marker.on('click', function(e){
+        L.DomEvent.stopPropagation(e);
+        fetch("http://localhost:8080/coordinates", {
+            method: "DELETE", 
+            headers : {"content-type":"application/json"}, 
+            body: JSON.stringify({"x":e.latlng.lat, "y":e.latlng.lng}) 
+        });
+        markers.removeLayer(this)
+        document.getElementById(li.id).outerHTML = "";
+        popup.setLatLng(e.latlng).setContent("You deleted " + e.latlng.toString()).openOn(mymap);
+    });
+    markers.addLayer(marker);
+    ulCoordinates.appendChild(li)
+}
+
+
+
+
+/**
+ * On a map click, add a bucket list item
+ * @param {*} e event
+ */
+async function onMapClick(e) {
+    await fetch("http://localhost:8080/coordinates", { 
+        method: "POST", 
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify({"x":e.latlng.lat, "y":e.latlng.lng, "name":"3", "visited": "false"})
+    });
+    const result = await fetch("http://localhost:8080/specificCoordinate?" + new URLSearchParams({
+        x: e.latlng.lat,
+        y: e.latlng.lng
+    }), { method: "GET"});
+    const coordinates = await result.json();
+    coordinates.forEach(t => {
+        addCoordinateToList(t);
+    });
+}
+mymap.on('click', onMapClick);
---
+++ server.js
@@ -0,0 +1,223 @@
+const {Client} = require("pg")
+const express = require("express")
+const app = express();
+app.use(express.json())
+app.use(express.static(__dirname));
+
+const client = new Client({
+    user: "postgres",
+    password: "penguin", 
+    host: "localhost",
+    port: "5440",
+    database: "griffin"
+})
+
+app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
+
+/**
+ * Accept http GET call and get all coordinates
+ */
+app.get('/coordinates', async (req, res) =>{
+    const rows = await readCoordinates();
+    res.setHeader("content-type", "application/json")
+    res.send(JSON.stringify(rows))
+})
+
+/**
+ * Accept http GET call and get a coordinate matching given x and y values
+ */
+app.get('/specificCoordinate', async (req, res) =>{
+    console.log("MADE IT")
+    const reqJson = req.query;
+    console.log(req.query);
+    const result = await getSpecificCoordinate(reqJson.x, reqJson.y);
+    console.log(result);
+    res.setHeader("content-type", "application/json")
+    res.send(JSON.stringify(result))
+})
+
+/**
+ * Accept http POST call and create a coordinate
+ */
+app.post('/coordinates', async (req, res) =>{
+    let result = {}
+    try{
+        const reqJson = req.body;
+        await addCoordinate(reqJson.x, reqJson.y, reqJson.name, reqJson.visited);
+        result.success = true;
+    }
+    catch(e){
+        result.success=false;
+    }
+    finally{
+        res.setHeader("content-type", "application/json")
+        res.send(JSON.stringify(result))
+    }
+})
+
+
+/**
+ * Accept http PUT call and update a coordinate
+ */
+app.put('/coordinates', async (req, res) =>{
+    let result = {}
+    try{
+        const reqJson = req.body;
+        await updateCoordinate(reqJson.id, reqJson.x, reqJson.y, reqJson.name, reqJson.visited);
+        result.success=true;
+    }
+    catch(e){
+        result.success=false;
+    }
+    finally{
+        res.setHeader("content-type", "application/json")
+        res.send(JSON.stringify(result))
+    }
+})
+
+/**
+ * Accept http DELETE call and delete coordinate
+ */
+app.delete('/coordinates', async (req, res) =>{
+    let result = {}
+    try{
+        const reqJson = req.body;
+        await deleteCoordinate(reqJson.x, reqJson.y);
+        result.success=true;
+    }
+    catch(e){
+        result.success=false;   
+    }
+    finally{
+        res.setHeader("content-type", "application/json")
+        res.send(JSON.stringify(result))
+    }
+})
+
+// listen to port 8080
+app.listen(8080, () => console.log("Web server is listening on port 8080"))
+
+
+execute()
+
+/**
+ * Start the program, connect to database and test CRUD
+ */
+async function execute(){
+    await connectToDB()
+    console.log("connection successful")
+    var coordinates = await readCoordinates();
+    console.table(coordinates);
+    const sucCreate = await addCoordinate(-1,-1,'test',false);
+    console.log(`Add was ${sucCreate}`);
+    coordinates = await readCoordinates();
+    console.table(coordinates);
+    const sucDelete = await deleteCoordinate(-1, -1);
+    console.log(`Delete was ${sucDelete}`);
+    coordinates = await readCoordinates();
+    console.table(coordinates);
+}
+
+/**
+ * Connect to the database
+ */
+async function connectToDB(){
+    try {
+        await client.connect();
+    }
+    catch(e){
+        console.error(`Connection failed ${e}`)
+    }
+}
+
+/**
+ * Retrieving the bucket list from the database
+ * @return {Array} true if we get the bucket list, false otherwise
+ */
+async function readCoordinates(){
+    try {
+        const results = await client.query("SELECT * FROM coordinates")
+        return results.rows;
+    }
+    catch(e){
+        console.error(`read failed ${e}`)
+        return [];
+    }
+}
+
+
+/**
+ * Get an item or items that match and x and y coordinate
+ * @param {*} x x coordinate of item
+ * @param {*} y y coordinate of item
+ * @return {Array} true if we get a coordinate from the bucket list, false otherwise
+ */
+async function getSpecificCoordinate(x, y){
+    try {
+        console.log(x + " " + y)
+        const results = await client.query("SELECT * FROM coordinates WHERE x = " + x + " AND y = " + y);
+        return results.rows;
+    }
+    catch(e){
+        console.error(`get failed ${e}`)
+        return [];
+    }
+}
+
+
+
+/**
+ * Add item to bucket list
+ * @param {*} x x coordinate
+ * @param {*} y y coordinate
+ * @param {String} name name of place
+ * @param {boolean} visited whether the place has been visited or not
+ * @return {boolean} true if successfully added, false otherwise
+ */
+async function addCoordinate(x, y, name, visited){
+    try {
+        console.log("add: " + x + " " + y)
+        await client.query("INSERT INTO coordinates(x,y,name,visited) VALUES (" + x + "," + y + ",'" + name + "'," + visited + ")");
+        return true;
+    }
+    catch(e){
+        return false;
+    }
+}
+
+/**
+ * Update item in the bucket list
+ * @param {int} id new id
+ * @param {*} x new x coordinate
+ * @param {*} y new y coordinate
+ * @param {String} name new name
+ * @param {bool} visited new visited
+ * @return {boolean} true if successfully updated, false otherwise
+ */
+async function updateCoordinate(id, x, y, name, visited){
+    try {
+        await client.query("UPDATE coordinates SET x = " + x + ", y = " + y +", name = '" + name + "', visited = " + visited + " WHERE x = " + x + " AND y = " + y);
+        return true;
+    }
+    catch(e){
+        return false;
+    }
+}
+
+
+/**
+ * Remove an item from the bucket list
+ * @param {*} x x coordiate of item to remove
+ * @param {*} y y coordinate of item to remove
+ * @return {boolean} true if successfully deleted, false otherwise
+ */
+async function deleteCoordinate(x, y){
+    try {
+        console.log("delete" + x + ", " + y);
+        await client.query("delete from coordinates where x = " + x + " AND y =" + y);
+        return true;
+    }
+    catch(e){
+        return false;
+    }
+}
